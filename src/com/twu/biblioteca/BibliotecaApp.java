51b3aec98d28f7f75d7093b54ad8376837ccbd99
Asu <suqin.zheng@thoughtworks.com>
2020-07-30 15:42:01 +0800

    [Asu] refactor: Print welcomemessage

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+        printWelcomeMessage();
+    }
+
+    public static void printWelcomeMessage() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 }
