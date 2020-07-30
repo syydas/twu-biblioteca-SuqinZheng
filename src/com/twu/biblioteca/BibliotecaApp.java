b5a6a8d9abb9d09c970ef8de57a65e90db1c8435
Asu <suqin.zheng@thoughtworks.com>
2020-07-30 16:33:39 +0800

    [Asu] feat-1.2: display a list of all books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,21 @@
 package com.twu.biblioteca;
 
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         printWelcomeMessage();
+        displayBookList();
     }
 
     public static void printWelcomeMessage() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    public static void displayBookList() {
+        String[] bookList = {"book1", "book2", "book3", "..."};
+        for (String book : bookList) {
+            System.out.println(book);
+        }
     }
 }
