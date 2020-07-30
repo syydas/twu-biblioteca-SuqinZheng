4ad90b7d519961cf453f97d2dd0eb1acb180a94b
Asu <suqin.zheng@thoughtworks.com>
2020-07-30 17:30:52 +0800

    [Asu] feat-1.4: add a main menu of options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -7,27 +7,41 @@ import com.twu.biblioteca.repositories.BookRepository;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BibliotecaApp {
+    public static BookRepository bookRepository = new BookRepository();
 
     public static void main(String[] args) {
-        BookRepository bookRepository = new BookRepository();
         List<Book> bookList = new ArrayList<>();
         bookList.add(new Book("book1", "authorA", Year.of(1995)));
         bookList.add(new Book("book2", "authorB", Year.of(1991)));
         bookList.add(new Book("book3", "authorC", Year.of(1997)));
         bookRepository.setBookList(bookList);
         printWelcomeMessage();
-        displayBookList(bookRepository);
+        menu();
     }
 
     public static void printWelcomeMessage() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 
-    public static void displayBookList(BookRepository bookRepository) {
+    public static void displayBookList() {
         for (Book book : bookRepository.getBookList()) {
             System.out.println(book);
+        }
+    }
+
+    public static void menu() {
+        System.out.println("Enter the number you want to choose");
+        System.out.println("1. List of books");
+        Scanner printItem = new Scanner(System.in);
+        String choice = printItem.next();
+        if ("1".equals(choice)) {
+            displayBookList();
+        } else {
+            System.out.println("Sorry, you may have entered a wrong number, please check and enter again");
+            menu();
         }
     }
 }
